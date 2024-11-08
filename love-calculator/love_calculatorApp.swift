eff7e2a886af0e4f42bd0d38c60d0bac2b85046c
Snow Yáng <ytm199891@gmail.com>
2024-11-08 16:20:44 +0800

    Initial Commit

---
+++ love-calculator/love_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  love_calculatorApp.swift
+//  love-calculator
+//
+//  Created by S on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct love_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
